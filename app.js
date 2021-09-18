b7c53c4443ef22c2a47084cced99d07db8f7e48a
d0561910 <d0561910@mail.fcu.edu.tw>
2021-09-18 22:17:36 +0800

    update

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const homeRouter = require('./routes/home');
+const contentRouter = require('./routes/content');
 
 const app = express();
 
@@ -20,9 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/home', homeRouter);
+app.use('/', contentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/home.js
+++ routes/home.js
@@ -1,38 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-// router.get("/", function (req, res, next) {
-//   const siderItemList = [
-//     { name: "Dashboard", icon: "la-igloo", link: "./Dashboard" },
-//     { name: "Customers", icon: "la-users", link: "./Customers" },
-//     { name: "Projects", icon: "la-clipboard-list", link: "./Projects" },
-//     { name: "Orders", icon: "la-shopping-bag", link: "./Orders" },
-//     { name: "Inventory", icon: "la-receipt", link: "./Inventory" },
-//     { name: "Accounts", icon: "la-user-circle", link: "./Accounts" },
-//     { name: "Tasks", icon: "la-clipboard-list", link: "./Tasks" },
-//   ];
-
-//   const active = "Dashboard";
-//   res.render("home", { siderItemList, active });
-// });
+router.get("/", function (req, res, next) {
+  const active = "Dashboard";
+  const siderItemList = [
+    { name: "Dashboard", icon: "la-igloo", link: "/home" },
+    { name: "DayNightMode", icon: "la-moon", link: "/home/DayNightMode" },
+  ];
+  
+  res.render("home", { siderItemList, active });
+});
 
 router.get("/:routerId", function (req, res, next) {
   let routerId = req.params?.routerId;
 
   const siderItemList = [
-    { name: "Dashboard", icon: "la-igloo", link: "./Dashboard" },
-    // { name: "Customers", icon: "la-users", link: "./Customers" },
-    // { name: "Projects", icon: "la-clipboard-list", link: "./Projects" },
-    // { name: "Orders", icon: "la-shopping-bag", link: "./Orders" },
-    // { name: "Inventory", icon: "la-receipt", link: "./Inventory" },
-    // { name: "Accounts", icon: "la-user-circle", link: "./Accounts" },
-    // { name: "Tasks", icon: "la-clipboard-list", link: "./Tasks" },
-    { name: "DayNightMode", icon: "la-moon", link: "./DayNightMode" },
+    { name: "Dashboard", icon: "la-igloo", link: "/home" },
+    { name: "DayNightMode", icon: "la-moon", link: "/home/DayNightMode" },
   ];
 
   const active = routerId;
-  const pageRender = routerId === "Dashboard" ? "home" : routerId;
+  const pageRender = routerId;
 
   res.render(pageRender, { siderItemList, active });
 });
