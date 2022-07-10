af9a3d9bb9aac07272efd4f46e3d96495761570a
Charles Sin <charlessin1993@gmail.com>
2022-07-10 15:17:37 +0800

    update SEO tag

--- routes/category.js
+++ routes/category.js
@@ -9,9 +9,14 @@ router.get("/", (req, res, next) => {
 
   for (const [key, value] of Object.entries(bookmarkJson)) {
     for (const [keys, values] of Object.entries(value)) {
-      bookmark.push(values.data);
+      values.data.linkTitle && values.data.linkTitle != "Facebook"
+        ? bookmark.push(values.data)
+        : null;
     }
   }
+
+  // Random get 10 article posts for render.
+  bookmark = bookmark.sort(() => Math.random() - Math.random()).slice(0, 12);
 
   res.render("category", { bookmarks: bookmark });
 });
